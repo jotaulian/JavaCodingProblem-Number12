8afda750a638987d54bac4d17e2ec0c5b4ea9abd
jotaulian <juluegglepalomo@gmail.com>
2020-12-18 12:04:11 +0100

    Java coding problems

---
+++ src/Mayor_Ej12.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Mayor_Ej12
+{
+
+	public static void main(String[] args)
+	{
+		Scanner teclado = new Scanner(System.in);
+		String cadenas[] = new String[5];
+		String base = null;
+		for(int i=0;i<cadenas.length;i++) {
+			System.out.println("Dame una palabra: ");
+			cadenas[i] = teclado.nextLine();
+		}
+		teclado.close();
+		base = cadenas[0];
+		for(int i=0;i<cadenas.length;i++) {
+			if(cadenas[i].compareTo(base)>0) {
+				base = cadenas[i];
+			}
+		}
+		System.out.println("la cadena alfabeticamente mayor es: " + base);
+	}
+
+}
+
+//Realizar un programa que lea 5 cadenas y nos diga cuál es la mayor de
+//ellas alfabéticamente.
